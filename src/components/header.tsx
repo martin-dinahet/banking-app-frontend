56212d97ddfe744e9a8dde9fa58e46e240fee1a4
Martin <martin.dinahet@proton.me>
2025-03-24 14:16:00 +0100

    feature: added header & theme toggle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./theme-toggle";
+import { BadgeDollarSign, Plus } from "lucide-react";
 
 export const Header: React.FC = () => {
   return (
     <>
-      <header>hello from header</header>
+      <header className="sticky top-0 flex p-3 justify-between">
+        <Button variant="ghost" asChild>
+          <div>
+            <BadgeDollarSign />
+            <NavLink to="/" className="font-semibold text-lg">
+              Banking app
+            </NavLink>
+          </div>
+        </Button>
+        <div>
+          {/* TODO replace this with the "New Expense" component */}
+          <Button variant="default" asChild>
+            <div>
+              <Plus /> New Expense
+            </div>
+          </Button>
+          <ThemeToggle />
+        </div>
+      </header>
     </>
   );
 };
--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,15 +3,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const MainLayout: React.FC = () => {
   return (
     <>
-      <div className="min-h-screen">
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
+      <ThemeProvider>
+        <div className="min-h-screen">
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+      </ThemeProvider>
     </>
   );
 };
